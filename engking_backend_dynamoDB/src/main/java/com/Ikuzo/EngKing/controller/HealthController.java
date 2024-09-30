62274bc1141125653528ae386f41a395f7acd469
kjh997788 <kjh997788@naver.com>
2024-09-30 16:07:39 +0900

    Update HealthController.java

--- engking_backend_dynamoDB/src/main/java/com/Ikuzo/EngKing/controller/HealthController.java
+++ engking_backend_dynamoDB/src/main/java/com/Ikuzo/EngKing/controller/HealthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
-@RequestMapping("/health")
+@RequestMapping("/status")
 public class HealthController {
 
     @GetMapping("")
